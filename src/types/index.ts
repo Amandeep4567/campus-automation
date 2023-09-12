82e1ba2a57598fb228076ae390d73ca060218d39
Amandeep Kalakar <amankumaradk@gmail.com>
2023-09-12 21:32:03 +0530

    [feat] outpass page created

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,10 @@ export interface CustomButtonProps {
   color: string
   fillColor: string
 }
+
+export interface CustomInputProps {
+  type: string
+  placeholder?: string
+  label?: string
+  containerStyle?: string
+}
